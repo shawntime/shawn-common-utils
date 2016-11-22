a3731347b72714f593739e98493fa9c73a551525
shma1664 <616059480@qq.com>
2016-11-22 18:23:10 +0800

    redis分布式锁实现

---
+++ src/main/java/com/shawntime/utils/lock/RedisLockInterceptor.java
@@ -0,0 +1,110 @@
+package com.shawntime.utils.lock;
+
+import java.lang.reflect.Method;
+
+import com.google.common.base.Joiner;
+import com.shawntime.utils.cache.redis.SpringRedisHelper;
+import com.shawntime.utils.lock.spelkey.KeySpELAdviceSupport;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IDEA
+ * User: mashaohua
+ * Date: 2016-09-28 18:08
+ * Desc:
+ */
+@Aspect
+@Component
+public class RedisLockInterceptor extends KeySpELAdviceSupport {
+
+    @Pointcut("@annotation(com.shawntime.utils.lock.RedisLockable)")
+    public void pointcut(){}
+
+    @Around("pointcut()")
+    public Object doAround(ProceedingJoinPoint point) throws Throwable{
+
+        MethodSignature methodSignature = (MethodSignature) point.getSignature();
+        Method targetMethod = AopUtils.getMostSpecificMethod(methodSignature.getMethod(), point.getTarget().getClass());
+        String targetName = point.getTarget().getClass().getName();
+        String methodName = point.getSignature().getName();
+        Object target = point.getTarget();
+        Object[] arguments = point.getArgs();
+
+        RedisLockable redisLock = targetMethod.getAnnotation(RedisLockable.class);
+        long expire = redisLock.expiration();
+        String redisKey = getLockKey(redisLock, targetMethod, targetName, methodName, target, arguments);
+        boolean isLock = lock(redisKey, expire);
+        if(isLock) {
+            long startTime = System.currentTimeMillis();
+            try {
+                return point.proceed();
+            } finally {
+                long parseTime = System.currentTimeMillis() - startTime;
+                if(parseTime <= expire * 1000) {
+                    unLock(redisKey);
+                }
+            }
+        } else {
+            throw new RuntimeException("您的操作太频繁，请稍后再试");
+        }
+    }
+
+    private String getLockKey(RedisLockable redisLock, Method targetMethod, String targetName, String methodName, Object target, Object[] arguments) {
+
+        String[] keys = redisLock.key();
+        String prefix = redisLock.prefix();
+        StringBuilder sb = new StringBuilder("lock.");
+        if(StringUtils.isEmpty(prefix)) {
+            sb.append(targetName).append(".").append(methodName);
+        } else {
+            sb.append(prefix);
+        }
+        if(keys != null) {
+            String keyStr = Joiner.on("+ '.' +").skipNulls().join(keys);
+            SpELOperationContext context = getOperationContext(targetMethod, arguments, target, target.getClass());
+            Object key = generateKey(keyStr, context);
+            sb.append("#").append(key);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 加锁
+     * @param key redis key
+     * @param expire 过期时间，单位秒
+     * @return true:加锁成功，false，加锁失败
+     */
+    private boolean lock(String key, long expire) {
+
+        long value = System.currentTimeMillis() + expire * 1000;
+        boolean status = SpringRedisHelper.setNX(key, value);
+
+        if(status) {
+            return true;
+        }
+
+        long oldExpireTime = SpringRedisHelper.get(key, Long.class);
+        if(oldExpireTime < System.currentTimeMillis()) {
+            //超时
+            long newExpireTime = System.currentTimeMillis() + expire * 1000;
+            long currentExpireTime = SpringRedisHelper.getSet(key, newExpireTime, Long.class);
+            if(currentExpireTime == oldExpireTime) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private void unLock(String key) {
+        SpringRedisHelper.delete(key);
+    }
+
+}
---
+++ src/main/java/com/shawntime/utils/lock/spelkey/ExpressionEvaluator.java
@@ -0,0 +1,130 @@
+package com.shawntime.utils.lock.spelkey;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Created by IDEA
+ * User: mashaohua
+ * Date: 2016-10-12 10:07
+ * Desc:
+ */
+class ExpressionEvaluator {
+
+    /**
+     * Indicate that there is no result variable.
+     */
+    public static final Object NO_RESULT = new Object();
+
+    /**
+     * Indicate that the result variable cannot be used at all.
+     */
+    public static final Object RESULT_UNAVAILABLE = new Object();
+
+    /**
+     * The name of the variable holding the result object.
+     */
+    public static final String RESULT_VARIABLE = "result";
+
+
+    private final SpelExpressionParser parser = new SpelExpressionParser();
+
+    // shared param discoverer since it caches data internally
+    private final ParameterNameDiscoverer paramNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+
+    private final Map<ExpressionKey, Expression> keyCache = new ConcurrentHashMap<ExpressionKey, Expression>(64);
+
+    private final Map<MethodCacheKey, Method> targetMethodCache = new ConcurrentHashMap<MethodCacheKey, Method>(64);
+
+
+    /**
+     * Create an {@link EvaluationContext} without a return value.
+     * @see #createEvaluationContext(Method, Object[], Object, Class, Object)
+     */
+    public EvaluationContext createEvaluationContext(Method method, Object[] args, Object target, Class<?> targetClass) {
+
+        return createEvaluationContext(method, args, target, targetClass, NO_RESULT);
+    }
+
+    /**
+     * Create an {@link EvaluationContext}.
+     * @param method the method
+     * @param args the method arguments
+     * @param target the target object
+     * @param targetClass the target class
+     * @param result the return value (can be {@code null}) or
+     * {@link #NO_RESULT} if there is no return at this time
+     * @return the evaluation context
+     */
+    public EvaluationContext createEvaluationContext(Method method, Object[] args, Object target, Class<?> targetClass, Object result) {
+
+        KeySpELExpressionRootObject rootObject = new KeySpELExpressionRootObject(method, args, target, targetClass);
+        KeySpELEvaluationContext evaluationContext = new KeySpELEvaluationContext(rootObject,
+                this.paramNameDiscoverer, method, args, targetClass, this.targetMethodCache);
+        if (result == RESULT_UNAVAILABLE) {
+            evaluationContext.addUnavailableVariable(RESULT_VARIABLE);
+        }
+        else if (result != NO_RESULT) {
+            evaluationContext.setVariable(RESULT_VARIABLE, result);
+        }
+        return evaluationContext;
+    }
+
+    public Object key(String keyExpression, MethodCacheKey methodKey, EvaluationContext evalContext) {
+        return getExpression(this.keyCache, keyExpression, methodKey).getValue(evalContext);
+    }
+
+    private Expression getExpression(Map<ExpressionKey, Expression> cache, String expression, MethodCacheKey methodKey) {
+        ExpressionKey key = createKey(methodKey, expression);
+        Expression expr = cache.get(key);
+        if (expr == null) {
+            expr = this.parser.parseExpression(expression);
+            cache.put(key, expr);
+        }
+        return expr;
+    }
+
+    private ExpressionKey createKey(MethodCacheKey methodCacheKey, String expression) {
+        return new ExpressionKey(methodCacheKey, expression);
+    }
+
+
+    private static class ExpressionKey {
+
+        private final MethodCacheKey methodCacheKey;
+
+        private final String expression;
+
+        public ExpressionKey(MethodCacheKey methodCacheKey, String expression) {
+            this.methodCacheKey = methodCacheKey;
+            this.expression = expression;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) {
+                return true;
+            }
+            if (!(other instanceof ExpressionKey)) {
+                return false;
+            }
+            ExpressionKey otherKey = (ExpressionKey) other;
+            return (this.methodCacheKey.equals(otherKey.methodCacheKey) &&
+                    ObjectUtils.nullSafeEquals(this.expression, otherKey.expression));
+        }
+
+        @Override
+        public int hashCode() {
+            return this.methodCacheKey.hashCode() * 29 + (this.expression != null ? this.expression.hashCode() : 0);
+        }
+    }
+
+}
