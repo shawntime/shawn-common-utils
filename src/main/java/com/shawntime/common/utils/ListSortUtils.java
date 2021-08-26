848ea06b8fb5d7a446c95f4318a0e87b2d143fb9
蓝汀华韶 <shma1664@gmail.com>
2021-08-26 15:53:50 +0800

    Update ListSortUtils.java
    支持list集合中包含重复数据逻辑

--- src/main/java/com/shawntime/common/utils/ListSortUtils.java
+++ src/main/java/com/shawntime/common/utils/ListSortUtils.java
@@ -1,13 +1,19 @@
 package com.shawntime.common.utils;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.TreeMultimap;
 import org.apache.commons.collections4.CollectionUtils;
 
 public final class ListSortUtils {
@@ -40,6 +46,30 @@ public final class ListSortUtils {
         return resultMap.values().stream().collect(Collectors.toList());
     }
 
+    public static <T> List<T> sortByMultiList(List<Integer> sortFieldList,
+                                              List<T> dataList,
+                                              Function<T, Integer> function) {
+        if (CollectionUtils.isEmpty(sortFieldList) || CollectionUtils.isEmpty(dataList)) {
+            return dataList;
+        }
+        int maxSize = sortFieldList.size() + dataList.size() + 1;
+        Map<Integer, Integer> sortIndexMap = new HashMap<>();
+        for (int sortField : sortFieldList) {
+            sortIndexMap.put(sortField, maxSize--);
+        }
+        Multimap<Integer, T> resultMap = TreeMultimap.create(Ordering.natural().reversed(), Ordering.usingToString());
+        for (T data : dataList) {
+            Integer fieldName = function.apply(data);
+            int index = sortIndexMap.getOrDefault(fieldName, 0);
+            if (index > 0) {
+                resultMap.put(index, data);
+            } else {
+                resultMap.put(maxSize--, data);
+            }
+        }
+        return resultMap.values().stream().collect(Collectors.toList());
+    }
+
     public static <T> void swap(List<T> list, int index1, int index2) {
         if (CollectionUtils.isEmpty(list)) {
             return;
@@ -50,6 +80,48 @@ public final class ListSortUtils {
         T data = list.get(index1);
         list.set(index1, list.get(index2));
         list.set(index2, data);
+    }
+
+    public static <T> void move(List<T> list, T data, int index) {
+        if (CollectionUtils.isEmpty(list)) {
+            return;
+        }
+        boolean isRemove = list.remove(data);
+        if (!isRemove) {
+            return;
+        }
+        list.add(index, data);
+    }
+
+    /**
+     * 集合按照groupNum个数，集合内的function值尽量不重复
+     */
+    public static <T> List<T> groupDistinctSortList(int groupNum,
+                                                    List<T> dataList,
+                                                    Function<T, Integer> function) {
+        List<T> sortList = new ArrayList<>();
+        while (true) {
+            List<T> groupList = new ArrayList<>();
+            Set<Integer> groupIds = new HashSet<>();
+            for (T data : dataList) {
+                if (groupList.size() >= groupNum) {
+                    break;
+                }
+                Integer groupId = function.apply(data);
+                if (groupIds.contains(groupId)) {
+                    continue;
+                }
+                groupList.add(data);
+                groupIds.add(groupId);
+            }
+            if (groupList.size() < groupNum) {
+                break;
+            }
+            sortList.addAll(groupList);
+            dataList.removeAll(groupList);
+        }
+        sortList.addAll(dataList);
+        return sortList;
     }
 
 }
